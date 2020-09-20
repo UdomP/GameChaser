80b604e41688e689f44de48b1016ccd50cfad3a9
AlmuHusan <38337359+AlmuHusan@users.noreply.github.com>
2020-09-20 11:54:16 -0400

    create react app setup

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+
+
+function App() {
+  return (
+  	
+    <div className="App">
+        <header >
+          
+                <h1 style={{display:"inline", alignItems:"center"}}>GAME CHASERS!</h1>
+                
+            
+              <label style={{ display:"inline",float: "right"}}>Region: NA</label>
+        </header>
+        <body className="App-body">
+        
+          
+      
+      
+          <a className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"> Click here for help setting up React!
+          </a>
+          
+          <p>
+            We're doing great team, keep it up!
+          </p>
+          
+          <img src="home.gif" alt="" width="500" height="333"/>
+          
+        </body>
+    </div>
+  );
+}
+
+export default App;
