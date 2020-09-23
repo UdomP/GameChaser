31740c668c29662c39c833d1edf7f848f8affb04
Jeff <71464201+Jellyon@users.noreply.github.com>
2020-09-23 18:25:14 -0400

    UI updated
    Updated the UI a little bit, added a button, some classes to App.css, and organized the existing code a little

--- src/App.js
+++ src/App.js
@@ -1,37 +1,52 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import ToggleButton from 'react-bootstrap/ToggleButton';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
-
-
+function btPress() {
+    alert('Code to do here!');
+}
 function App() {
+
+    
   return (
   	
     <div className="App">
-        <header >
+          <div className= "App-header" >
           
-                <h1 style={{display:"inline", alignItems:"center"}}>GAME CHASERS!</h1>
+                <h1 style={{display:"inline", alignItems:"center"}}> GAME CHASERS!</h1>
                 
             
-              <label style={{ display:"inline",float: "right"}}>Region: NA</label>
-        </header>
-        <body >
-        <a >Search by game</a>
-        <input type="text"/>
-        <a >Search by user</a>
-        <input type="text"/>
-        
-          <br/>
-        <table style={{width:"100%",border: "1px solid black",borderCollapse: "collapse"}} >
-            <tr>
-              <th>Game</th>
-              <th>Name</th>
-              <th>Size</th>
-              <th>Time</th>
-              <th>Notes</th>
-            </tr>
-        </table> 
-          
-        </body>
-    </div>
+              <label style={{ display: "inline", float: "right" }}>Region: NA</label>
+
+          </div>{/*End of header*/}
+
+          <div className="App-searchbar">
+             
+            <a >Search by game</a>
+            <input type="text" />
+              &emsp;
+            <a >Search by user</a>
+            <input type="text" />
+              &emsp;
+            <Button variant="secondary" size="lg" onClick={btPress} > Search </Button>{''}
+          </div> {/* End of searchbar */}
+
+          <div className="App-table">
+              
+              <table style={{ width: "100%", border: "1px solid black", borderCollapse: "collapse" }} >
+                  <tr>
+                      <th>Game</th>
+                      <th>Name</th>
+                      <th>Size</th>
+                      <th>Time</th>
+                      <th>Notes</th>
+                  </tr>
+              </table>
+           </div>
+
+        </div> /* End of App*/
   );
 }
 
