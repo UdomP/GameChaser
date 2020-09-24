b47fa15bc7306dbfb91af361660745818ce898d5
Jeff <71464201+Jellyon@users.noreply.github.com>
2020-09-23 21:03:31 -0400

    Stuck for now
    Updated the button and text inputs to send values to a variable. Not sure how to display the results in the table. Debating whether or not to clear the inputs upon search, seems like extra, but I'd like to do it. Will pick up tomorrow learning how to deal with table inputs in React

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import React from 'react';
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import ToggleButton from 'react-bootstrap/ToggleButton';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function btPress() {
-    alert('Code to do here!');
-}
+
+
 function App() {
 
-    
+    const [sg, setGame] = useState(' ');
+    const [su, setUser] = useState(' ');
+
+    function btPress() {//sample code for testing purposes
+
+        if ((sg == ' ') && (su == ' ')) {
+            alert("Displaying all queries");
+        }
+        else if ((sg != ' ') && (su == ' ')) {
+            alert("Search results for: " + sg + "; All users");
+        }
+        else if ((sg == ' ') && (su != ' ')) {
+            alert("Search results for User: " + su);
+        }
+        else {
+            alert("Search results for: " + sg + "; User: " + su);
+        }
+        setGame(' '); setUser(' ');
+
+         //displaying results and clear the text inputs is where I'm stuck with React    
+
+    }
   return (
   	
     <div className="App">
@@ -22,11 +43,11 @@ function App() {
 
           <div className="App-searchbar">
              
-            Search by game-
-            <input type="text" />
+              Search by game-
+            <input type="resetText" onChange={ge => setGame(ge.target.value)} />
               &emsp;
             Search by user- 
-            <input type="text" />
+            <input type="text" onChange={gc => setUser(gc.target.value)} />
               &emsp;
             <Button variant="secondary" size="lg" onClick={btPress} > Search </Button>{''}
           </div> {/* End of searchbar */}
